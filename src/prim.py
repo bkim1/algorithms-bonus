b21d4bed9fc528ce42383736da433ee33938f341
Branden Kim <brandenk0@gmail.com>
2017-12-08 16:00:29 -0500

    Refactor prim

--- src/prim.py
+++ src/prim.py
@@ -1,22 +1,22 @@
 from pythonds.graphs import PriorityQueue, Graph, Vertex
 
-def prim(Graph, source):
+def prim(graph, source):
     pq = PriorityQueue()
     source.setDistance(0)
     total_weight = 0
-    for n in Graph:
-        n.setDistance(sys.maxsize)
+    for n in graph:
+        n.setDistance(float('Inf'))
         n.setPred(None)
-    pq.buildHeap([n.getDistance(), n) for n in Graph]) #(distance, node)
+    pq.buildHeap([(n.getDistance(), n) for n in Graph]) #(distance, node)
 
-    while (pq):
-        currNode = pq.delMin() #get min value from priority queue
-        for adj in currNode.getConnections(): #for all adjacacent nodes to the current node
-            weight = currNode.getWeight(adj) #weight of adj node
+    while pq:
+        curr_node = pq.delMin() #get min value from priority queue
+        for adj in curr_node.getConnections(): #for all adjacacent nodes to the current node
+            weight = curr_node.getWeight(adj) #weight of adj node
             if adj in pq: #if it is in the pq ie not already in the mst
                 if weight < adj.getDistance(): #new weight is minimum
-                    adj.setPred(currNode) #set the previous node of adjacent node
+                    adj.setPred(curr_node) #set the previous node of adjacent node
                     adj.setDistance(weight) #set weight
                     total_weight += weight
                     pq.decreaseKey(adj, weight) #take the adj node out of the pq
-return total_weight
+    return total_weight
